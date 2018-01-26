fb283f127b3571218c3350b0b7a5d381ab4459df
cserteGT3 <cserteGT3@users.noreply.github.com>
2018-01-26 23:41:56 +0100

    Copypasted every function with exponential and quadratic approximation

--- rblc.jl
+++ rblc.jl
@@ -1,3 +1,15 @@
+numchop{T <: Real}(x::T, δ = eps(T)) = abs(x) > δ ? x : zero(T)
+
+function numchop{T <: Real}(x::Complex{T}, δ = eps(T))
+  ichop = numchop(imag(x), δ)
+  rchop = numchop(real(x), δ)
+  if ichop == zero(T)
+    rchop
+  else
+    complex(rchop, ichop)
+  end
+end
+
 import Base.range
 struct RangeHolder{T}
   minimum::T
@@ -77,7 +89,7 @@ function createNNinput(df,sr::RangeHolder{Integer},dr::RangeHolder{Date})
   return inputArray, outputArray
 end
 
-#Knet functions
+#Linear functions
 predict_lin(W,x)=W[1]*x+W[2]
 loss_lin(W,x,y)=sum(abs2,y-predict_lin(W,x))/ size(y,2)
 lossgrad_lin=grad(loss_lin)
@@ -114,13 +126,13 @@ function learnLinPars(iters,daf,lr,SR::RangeHolder{Integer},DR::RangeHolder{Date
         kthLoss+=loss_lin(W,inp[i],outp[i])
       end
       avgLoss=mean(kthLoss)
-      println("Average loss in $k-th iteration is: $avgLoss")
+      println("Average loss with linear approximation in $k-th iteration is: $avgLoss")
     end
   end
   return W
 end
 
-function subFromDate(w,d::Date,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+function linSubFromDate(w,d::Date,sr::RangeHolder{Integer},dr::RangeHolder{Date})
   fld=real2normDate(d,dr)
   if fld<0 || fld>1
     error("Float date is out of range: $fld")
@@ -128,22 +140,22 @@ function subFromDate(w,d::Date,sr::RangeHolder{Integer},dr::RangeHolder{Date})
   return invSub(predict_lin(w,fld),sr)
 end
 
-function subFromDate(w,fld::Real,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+function linSubFromDate(w,fld::Real,sr::RangeHolder{Integer},dr::RangeHolder{Date})
   if fld<0 || fld>1
     error("Float date is out of range: $fld")
   end
   return invSub(predict_lin(w,fld),sr)
 end
 
-function resLinNN(w,sr::RangeHolder{Integer},dr::RangeHolder{Date},df)
+function resultLinNN(w,sr::RangeHolder{Integer},dr::RangeHolder{Date},df)
   oriDates,oriSubs=readDF(df)
   flDates=collect(linspace(0,1,50))
   pDates=[norm2realDate(x,dr) for (i,x) in enumerate(flDates)]
-  pSubs=[subFromDate(w,x,sr,dr) for (i,x) in enumerate(flDates)]
+  pSubs=[linSubFromDate(w,x,sr,dr) for (i,x) in enumerate(flDates)]
   return pDates,pSubs,oriDates,oriSubs
 end
 
-function whenGivenSub(sub::Integer,w,s::RangeHolder{Integer},d::RangeHolder{Date})
+function linWhenGivenSub(sub::Integer,w,s::RangeHolder{Integer},d::RangeHolder{Date})
   if sub<s.minimum || sub>s.maximum
     error("Given sub is out of range!")
   end
@@ -155,7 +167,7 @@ function whenGivenSub(sub::Integer,w,s::RangeHolder{Integer},d::RangeHolder{Date
 end
 
 function plotLinNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df)
-  pD,pS,oD,oS=resLinNN(w,s,d,df);
+  pD,pS,oD,oS=resultLinNN(w,s,d,df);
   xt=d.minimum:Dates.Day(200):d.maximum
   yt=s.minimum:5000:s.maximum
   plot(pD,pS,legend=:bottomright,xticks=xt,yticks=yt,xlabel="Date",ylabel="Subscribers count",label="Predicted values",size=(800,600),dpi=200,title="Linear approximation")
@@ -163,7 +175,187 @@ function plotLinNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df)
 end
 
 function plotLinNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df,gS)
-  gD=whenGivenSub(gS,w,s,d)
+  gD=linWhenGivenSub(gS,w,s,d)
   plotLinNN(w,s,d,df)
+  scatter!([gD,gD],[gS,gS], label="Given subs")
+end
+
+#Exponential functions
+predict_exp(W,x)=W[1]*exp(x)+W[2]
+loss_exp(W,x,y)=sum(abs2,y-predict_exp(W,x))/ size(y,2)
+lossgrad_exp=grad(loss_exp)
+
+function learnExpPars(iters,daf,lr,SR::RangeHolder{Integer},DR::RangeHolder{Date};printNum=100)
+  W=rand(2)
+  inp,outp=createNNinput(daf,SR,DR)
+  start_loss=loss_exp(W,inp[1],outp[1])
+  println("Start loss is $start_loss.")
+  setSize=size(inp,1)
+  wSize=size(W,1)
+  for k in 1:iters
+    for j in 1:setSize
+      dw=lossgrad_exp(W,inp[j],outp[j])
+      for i in 1:wSize
+        W[i]-=lr*dw[i]
+      end
+    end
+    if k%printNum==0
+      kthLoss=0
+      for i in 1:setSize
+        kthLoss+=loss_exp(W,inp[i],outp[i])
+      end
+      avgLoss=mean(kthLoss)
+      println("Average loss with linear exponential in $k-th iteration is: $avgLoss")
+    end
+  end
+  return W
+end
+
+function expSubFromDate(w,d::Date,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+  fld=real2normDate(d,dr)
+  if fld<0 || fld>1
+    error("Float date is out of range: $fld")
+  end
+  return invSub(predict_exp(w,fld),sr)
+end
+
+function expSubFromDate(w,fld::Real,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+  if fld<0 || fld>1
+    error("Float date is out of range: $fld")
+  end
+  return invSub(predict_exp(w,fld),sr)
+end
+
+function resultExpNN(w,sr::RangeHolder{Integer},dr::RangeHolder{Date},df)
+  oriDates,oriSubs=readDF(df)
+  flDates=collect(linspace(0,1,50))
+  pDates=[norm2realDate(x,dr) for (i,x) in enumerate(flDates)]
+  pSubs=[expSubFromDate(w,x,sr,dr) for (i,x) in enumerate(flDates)]
+  return pDates,pSubs,oriDates,oriSubs
+end
+
+function expWhenGivenSub(sub::Integer,w,s::RangeHolder{Integer},d::RangeHolder{Date})
+  if sub<s.minimum || sub>s.maximum
+    error("Given sub is out of range!")
+  end
+  nS=normSub(sub,s)
+  preV=(nS-W[2])/W[1]
+  if preV<0
+    error("Argument of logarithmus naturalis is negative: $preV")
+  end
+  nD=log(preV)
+  rD=norm2realDate(nD,d)
+  println("Given $sub subscribers will be achieved on $rD.")
+  return rD
+end
+
+function plotExpNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df)
+  pD,pS,oD,oS=resultExpNN(w,s,d,df);
+  xt=d.minimum:Dates.Day(200):d.maximum
+  yt=s.minimum:5000:s.maximum
+  plot(pD,pS,legend=:bottomright,xticks=xt,yticks=yt,xlabel="Date",ylabel="Subscribers count",label="Predicted values",size=(800,600),dpi=200,title="Exponential approximation")
+  scatter!(oD,oS,label="Teaching set")
+end
+
+function plotExpNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df,gS)
+  gD=expWhenGivenSub(gS,w,s,d)
+  plotExpNN(w,s,d,df)
+  scatter!([gD,gD],[gS,gS], label="Given subs")
+end
+
+#Quadratic functions
+predict_quad(W,x)=W[1]*x^2+W[2]*x+W[3]
+loss_quad(W,x,y)=sum(abs2,y-predict_quad(W,x))/ size(y,2)
+lossgrad_quad=grad(loss_quad)
+
+function learnQuadPars(iters,daf,lr,SR::RangeHolder{Integer},DR::RangeHolder{Date};printNum=100)
+  W=rand(3)
+  inp,outp=createNNinput(daf,SR,DR)
+  start_loss=loss_quad(W,inp[1],outp[1])
+  println("Start loss is $start_loss.")
+  setSize=size(inp,1)
+  wSize=size(W,1)
+  for k in 1:iters
+    for j in 1:setSize
+      dw=lossgrad_quad(W,inp[j],outp[j])
+      for i in 1:wSize
+        W[i]-=lr*dw[i]
+      end
+    end
+    if k%printNum==0
+      kthLoss=0
+      for i in 1:setSize
+        kthLoss+=loss_quad(W,inp[i],outp[i])
+      end
+      avgLoss=mean(kthLoss)
+      println("Average loss with quadratic approximation in $k-th iteration is: $avgLoss")
+    end
+  end
+  return W
+end
+
+function quadSubFromDate(w,d::Date,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+  fld=real2normDate(d,dr)
+  if fld<0 || fld>1
+    error("Float date is out of range: $fld")
+  end
+  return invSub(predict_quad(w,fld),sr)
+end
+
+function quadSubFromDate(w,fld::Real,sr::RangeHolder{Integer},dr::RangeHolder{Date})
+  if fld<0 || fld>1
+    error("Float date is out of range: $fld")
+  end
+  return invSub(predict_quad(w,fld),sr)
+end
+
+function resultQuadNN(w,sr::RangeHolder{Integer},dr::RangeHolder{Date},df)
+  oriDates,oriSubs=readDF(df)
+  flDates=collect(linspace(0,1,50))
+  pDates=[norm2realDate(x,dr) for (i,x) in enumerate(flDates)]
+  pSubs=[quadSubFromDate(w,x,sr,dr) for (i,x) in enumerate(flDates)]
+  return pDates,pSubs,oriDates,oriSubs
+end
+
+function quadWhenGivenSub(sub::Integer,w,s::RangeHolder{Integer},d::RangeHolder{Date})
+  if sub<s.minimum || sub>s.maximum
+    error("Given sub is out of range!")
+  end
+  nS=normSub(sub,s)
+  rts=roots([w[3]-nS,w[2],w[1]])
+  realRts=map(numchop,rts)
+  boolVal=falses(size(realRts,1))
+  for (ind,val) in enumerate(realRts)
+    if val<=1 && val>=0
+      boolVal[ind]=true
+    end
+  end
+  sumOfbV=sum(boolVal)
+  if sumOfbV==0
+    error("Didn't find solution for quadratic equation: $realRts")
+  elseif sumOfbV==1
+    for (ind,val) in enumerate(boolVal)
+      if val
+        rd=norm2realDate(realRts[ind],d)
+        println("Given $sub subscribers will be achieved on $rd.")
+        return rd
+      end
+    end
+  else
+    error("Multiple valid solutions found for quadratic equation: $realRts")
+  end
+end
+
+function plotQuadNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df)
+  pD,pS,oD,oS=resultQuadNN(w,s,d,df);
+  xt=d.minimum:Dates.Day(200):d.maximum
+  yt=s.minimum:5000:s.maximum
+  plot(pD,pS,legend=:bottomright,xticks=xt,yticks=yt,xlabel="Date",ylabel="Subscribers count",label="Predicted values",size=(800,600),dpi=200,title="Quadratic approximation")
+  scatter!(oD,oS,label="Teaching set")
+end
+
+function plotQuadNN(w,s::RangeHolder{Integer},d::RangeHolder{Date},df,gS)
+  gD=quadWhenGivenSub(gS,w,s,d)
+  plotQuadNN(w,s,d,df)
   scatter!([gD,gD],[gS,gS], label="Given subs")
 end
